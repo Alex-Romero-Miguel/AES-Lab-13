1692f417af575bdb218812b628193202e3a5c71c
Alex-Romero-Miguel <alex.romero.miguel@estudiantat.upc.edu>
2025-09-25 19:51:18 +0200

    Create test_Lab_13.py

---
+++ test_Lab_13.py
@@ -0,0 +1,13 @@
+# código ejecutable (main)
+
+'''
+Al ser invocado desde la línea de comandos:
+
+python test Lab XX.py -c -f file -p 0x11d -k 0xe9e03576ec312b3698c3b6f37d49d770
+cifra el fichero file usando el polinomio 0x11d y la clave
+0xe9e03576ec312b3698c3b6f37d49d770 (en este caso sería una clave de 128 bits)
+
+python test Lab XX.py -d -f file -p 0x11d -k 0xe9e03576ec312b3698c3b6f37d49d770
+descifra el fichero file usando el polinomio 0x11d y la clave
+0xe9e03576ec312b3698c3b6f37d49d770
+'''
